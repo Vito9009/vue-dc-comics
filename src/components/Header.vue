3b38814f99a8e9d389bd2990e247f971cde26d58
Your Name <yan.vi193@gmail.com>
2022-01-27 16:30:27 +0100

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,21 @@
 <template>
   <header>
-      
+      <a href="#" class="navbar-icon">
+          <img src="../assets/img/dc-logo.png" alt="">
+      </a>
+
+      <ul class="my_navbar-container">
+          <li>Characters</li>
+          <li>Comics</li>
+          <li>Movies</li>
+          <li>TV</li>
+          <li>Games</li>
+          <li>Collectibles</li>
+          <li>Videos</li>
+          <li>Fans</li>
+          <li>News</li>
+          <li>Shop</li>
+      </ul>
   </header>
 </template>
 
@@ -14,6 +29,40 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+    header{
+        widows: 100%;
+        height: 150px;
+        display: flex;
+        background-color: rgb(255, 255, 255);
 
+        .navbar-icon{
+            width: 35%;
+            height: 100%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            img{
+                width: 100px;
+            }
+        }
+
+        .my_navbar-container{
+            list-style: none;
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            margin: 0;
+
+            li{
+                height: 100%;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                padding: 0 20px;
+                text-transform: uppercase;
+            }
+        }
+    }
 </style>
