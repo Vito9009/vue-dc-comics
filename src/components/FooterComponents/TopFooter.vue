d902c61e9dcd2f384971d3c1bd4919c04ba59e21
Your Name <yan.vi193@gmail.com>
2022-01-27 18:27:24 +0100

    top-footer

--- src/components/FooterComponents/TopFooter.vue
+++ src/components/FooterComponents/TopFooter.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="my_top-footer-container">
-      
+      <div class="test">
+        <TopFooterComponents />
+        <TopFooterComponents />
+        <TopFooterComponents />
+        <TopFooterComponents />
+        <TopFooterComponents />
+      </div>
+
+
   </div>
 </template>
 
 <script>
-
+import TopFooterComponents from './TopFooterComponents/TopFooterComponents.vue'
 
 export default {
   name: 'TopFooter',
   components: {
+      TopFooterComponents
   }
 }
 </script>
@@ -21,5 +30,17 @@ export default {
         background-color: $blueComics;
         height: 200px;
         width: 100%;
+                display: flex;
+        justify-content: center;
+        align-items: center;
+
+
+        .test{
+            width: 60%;
+                    display: flex;
+        justify-content: center;
+        align-items: center;
+
+        }
     }
 </style>
---
+++ src/components/FooterComponents/TopFooterComponents/TopFooterComponents.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="buy-comics-container">
+      <img src="../../../assets/img/buy-comics-digital-comics.png" alt="">
+      <span>digital comics</span>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'TopFooterComponents',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+    .buy-comics-container{
+        width: calc(100% / 5);
+        color: rgb(255, 255, 255);
+        text-transform: uppercase;
+        font-size: 15px;
+        display: flex;
+        justify-content: start;
+        align-items: center;
+
+
+        img{
+            height: 80px;
+            margin-right: 10px;
+        }
+    }
+</style>
